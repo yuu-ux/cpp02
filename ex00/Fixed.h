4338fb6cec97387a50305aff2ac95e5cd7562cc7
yuu-ux <yuugaehara@gmail.com>
2025-07-26 15:04:30 +0900

    :recycle: format google style guide

--- ex00/Fixed.h
+++ ex00/Fixed.h
@@ -1,18 +1,19 @@
 #ifndef FIXED_H
-# define FIXED_H
+#define FIXED_H
 #include <iostream>
 
 class Fixed {
-    public:
-        Fixed();
-        Fixed(const Fixed &other);
-        Fixed &operator=(const Fixed &fixed);
-        ~Fixed();
-        int getRawBits(void) const;
-        void setRawBits(int const raw);
-    private:
-        int fixed_point_num_;
-        static const int fractional_bits_ = 8;
+ public:
+  Fixed();
+  Fixed(const Fixed &other);
+  Fixed &operator=(const Fixed &fixed);
+  ~Fixed();
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+
+ private:
+  int fixed_point_num_;
+  static const int fractional_bits_ = 8;
 };
 
 #endif
