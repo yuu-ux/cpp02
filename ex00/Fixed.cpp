25b201680eef6fda720b064d4d348dcad4c4a07c
yuu-ux <yuugaehara@gmail.com>
2025-07-26 15:13:05 +0900

    :recycle: format for google style guide

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,32 +1,31 @@
 #include "Fixed.h"
 
 Fixed::Fixed() : fixed_point_num_(0) {
-    std::cout << "Default constructor called" << std::endl;
+  std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy) {
-    std::cout << "Copy constructor called" << std::endl;
-    this->fixed_point_num_ = copy.getRawBits();
+  std::cout << "Copy constructor called" << std::endl;
+  this->fixed_point_num_ = copy.getRawBits();
 }
 
 Fixed &Fixed::operator=(const Fixed &fixed) {
-    std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &fixed) {
-        this->fixed_point_num_ = fixed.getRawBits();
-    }
-    return *this;
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &fixed) {
+    this->fixed_point_num_ = fixed.getRawBits();
+  }
+  return *this;
 }
 
 Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
+  std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
-    return fixed_point_num_;
+int Fixed::getRawBits() const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return fixed_point_num_;
 }
 
 void Fixed::setRawBits(int const raw) {
-    fixed_point_num_ = raw;
+  fixed_point_num_ = raw;
 }
-
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,12 @@
 #include "Fixed.h"
 
-int main( void ) {
-    Fixed a;
-    Fixed b( a );
-    Fixed c;
-    c = b;
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
-    return 0;
+int main() {
+  const Fixed a;
+  const Fixed b(a);
+  Fixed c;
+  c = b;
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  return 0;
 }
